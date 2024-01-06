f3fb05a23a45d32127e0cc864f8c1ed69edd3db4
thusith <thusithanjula16@gmail.com>
2024-01-06 11:38:18 +0530

    Second Commit -Task 4

---
+++ src/CurrencyConverter.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+public class CurrencyConverter {
+    public class Main {
+        public static void main(String[] args) throws IOException {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter currency convert from:");
+            String convertFrom = sc.nextLine();
+            System.out.println("Enter currency convert to:");
+            String convertTo = sc.nextLine();
+            System.out.println("Enter quantity to convert:");
+            BigDecimal quantity = sc.nextBigDecimal();
+
+            String apiKey = "2bf5b45f9cf0a9a0580f5dfc16d94fcb";
+            String urlString = "https://api.exchangerate.host/list?access_key=" + apiKey + "&base=" + convertFrom.toUpperCase();
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(urlString).get().build();
+
+            Response response = client.newCall(request).execute();
+            String stringResponse = response.body().string();
+            System.out.println("API Response: " + stringResponse);
+
+            JSONObject jsonObject = new JSONObject(stringResponse);
+            JSONObject ratesObject = jsonObject.getJSONObject("rates");
+            BigDecimal rate = ratesObject.getBigDecimal(convertTo.toUpperCase());
+
+            BigDecimal result = rate.multiply(quantity);
+            System.out.println(result);
+        }
+    }
+}
